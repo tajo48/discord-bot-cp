4d175244948c050f57ea5e11e507a6e293ce76f0
tajo48 <55502906+tajo48@users.noreply.github.com>
2020-12-01 11:50:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ client.on("message", (message) => {
   if (command != "tutaj") return;
 
   // Set the date we're counting down to
-  var countDownDate = new Date("Dec 10, 2020 12:00:00").getTime();
+  var countDownDate = new Date("Dec 10, 2020 01:00:00").getTime();
 
   // Update the count down every 1 second
   var x = setInterval(function () {
